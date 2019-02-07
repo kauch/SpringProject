0104c2168ed125eaf83138b361ea177bd4b64c72
kauch <kst.fisenk@rambler.ru>
2019-02-07 20:09:55 +0300

    add project

---
+++ SbSecurityJdbcThymeleaf/src/main/java/com/netcracker/SbSecurityJdbcThymeleafApplication.java
@@ -0,0 +1,14 @@
+package com.netcracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbSecurityJdbcThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbSecurityJdbcThymeleafApplication.class, args);
+	}
+
+}
+
