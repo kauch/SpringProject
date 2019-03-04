63ce2d8288af12ee390f51fd31415cf398cd2df3
Kert <Kert@192.168.1.51>
2019-03-04 23:14:36 +0300

    add test

--- SbSecurityJdbcThymeleaf/src/test/java/com/netcracker/TestUserAndRoles.java
+++ SbSecurityJdbcThymeleaf/src/test/java/com/netcracker/TestUserAndRoles.java
@@ -1,27 +1,27 @@
 package com.netcracker;
 
-import java.util.HashSet;
-import java.util.Set;
+//import java.util.HashSet;
+//import java.util.Set;
 
 import org.junit.Test;
 
-import com.netcracker.model.Roles;
-import com.netcracker.model.Users;
+//import com.netcracker.model.Roles;
+//import com.netcracker.model.Users;
 
 public class TestUserAndRoles {
 
 	@Test
 	public void test() {
-		Roles testRole1 = new Roles(1L, "ROLE_ADMIN");
-		Roles testRole2 = new Roles(2L, "ROLE_USER");
-
-		Set<Roles> userSet = new HashSet<>();
-		userSet.add(testRole1);
-		Set<Roles> adminSet = new HashSet<>();
-		userSet.add(testRole2);
-
-		Users testUser1 = new Users(1L, "userCustomer", "123", true, userSet);
-		Users testUser2 = new Users(2L, "userAdmin", "123", true, adminSet);
+//		Roles testRole1 = new Roles(1L, "ROLE_ADMIN");
+//		Roles testRole2 = new Roles(2L, "ROLE_USER");
+//
+//		Set<Roles> userSet = new HashSet<>();
+//		userSet.add(testRole1);
+//		Set<Roles> adminSet = new HashSet<>();
+//		userSet.add(testRole2);
+//
+//		Users testUser1 = new Users(1L, "userCustomer", "123", true, userSet);
+//		Users testUser2 = new Users(2L, "userAdmin", "123", true, adminSet);
 
 	}
 
