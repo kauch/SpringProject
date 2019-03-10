f8b389c5e7058c8fab65884ebfe10a53eef2e361
Kert <Kert@192.168.1.51>
2019-03-11 01:20:46 +0300

    add repositories and ER diagram picture

--- SbSecurityJdbcThymeleaf/src/main/java/com/netcracker/repositories/AddressRepository.java
+++ SbSecurityJdbcThymeleaf/src/main/java/com/netcracker/repositories/AddressRepository.java
@@ -3,9 +3,9 @@ package com.netcracker.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.netcracker.model.Users;
+import com.netcracker.model.Address;
 
 @Repository
-public interface UsersRepository extends JpaRepository<Users, Long> {
-	public Users findByUserName(String userName);
+public interface AddressRepository extends JpaRepository<Address, Long> {
+
 }
--- SbSecurityJdbcThymeleaf/src/main/java/com/netcracker/repositories/ProductRepository.java
+++ SbSecurityJdbcThymeleaf/src/main/java/com/netcracker/repositories/ProductRepository.java
@@ -3,9 +3,9 @@ package com.netcracker.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.netcracker.model.Users;
+import com.netcracker.model.Product;
 
 @Repository
-public interface UsersRepository extends JpaRepository<Users, Long> {
-	public Users findByUserName(String userName);
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
 }
